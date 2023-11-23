3df59367c42ad879eaa3d1f9363ef55c0329dc01
Mikko Murhu <mikko.murhu@aalto.fi>
2023-11-23 20:13:06 +0200

    Assignment 2.11

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = ({person}) => {
   return(
@@ -57,6 +58,14 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
 
   const [nameFilter, setNameFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const names = persons.map(person => person.name)
 
